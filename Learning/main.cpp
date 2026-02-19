608ff4069baa5396f43ee2765eb09321faf89a83
Maciej Piechowski <maciej.piechowski.dev@gmail.com>
2026-02-19 13:23:31 +0100

    initialization

--- Learning/main.cpp
+++ Learning/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
 int main() {
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+
+    //Copy-initialization
+    //int width = 5;
+
+    // List-initialization (preferred in C++)
+    // int width {5}; OK
+    // int height {5.7}; Not OK, because 5.7 is not an int
+    // width = 5.7; OK, the double will be converted to int
+
     return 0;
 }
