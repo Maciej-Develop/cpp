59d6903af59f00ec513305057c844b5bf9d54794
Maciej Piechowski <maciej.piechowski.dev@gmail.com>
2026-02-19 13:42:00 +0100

    cout and cin

--- Learning/main.cpp
+++ Learning/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
 int main() {
-
     //Copy-initialization
     //int width = 5;
 
@@ -9,6 +8,15 @@ int main() {
     // int width {5}; OK
     // int height {5.7}; Not OK, because 5.7 is not an int
     // width = 5.7; OK, the double will be converted to int
+
+    // If multiple std::cout, prefer \n over std::endl (less flushes)
+    //std::cout << "Hello world!" << std::endl;
+
+    //
+    /*std::cout << "Enter a number: ";
+    int x{};
+    std::cin >> x;
+    std::cout << "You entered " << x << '\n';*/
 
     return 0;
 }
